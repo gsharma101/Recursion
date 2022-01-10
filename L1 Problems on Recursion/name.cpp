8a7031771fac307bf1ab6a818ed4566380fb98d1
gsharma101 <sharmag8121@gmail.com>
2022-01-11 02:19:43 +0530

    print your name n times

--- L1 Problems on Recursion/name.cpp
+++ L1 Problems on Recursion/name.cpp
@@ -1,8 +1,21 @@
 #include <bits\stdc++.h>
-
+// Program to print your name n times using recursion
 using namespace std;
 
+void name(int i, int n){
+    // Base Case
+    if(i > n)
+        return ;
+    
+    cout<<"Gaurav\n";
+    // Recrance relation
+    name(i+1,n);
+}
+
 int main(){
-    cout<<"Hello Word";
+    int n;
+    cout<<"How many time you want to print your name\n";
+    cin>>n;
+    name(1,n);
     return 0;
 }
