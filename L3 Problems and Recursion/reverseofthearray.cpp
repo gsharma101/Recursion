d4925fead25cddd5e4e7d5d040facaec54134461
gsharma101 <sharmag8121@gmail.com>
2022-01-26 23:31:21 +0530

    Lecture 3

---
+++ L3 Problems and Recursion/reverseofthearray.cpp
@@ -0,0 +1,18 @@
+#include <bits\stdc++.h>
+// Program to reverse the array
+using namespace std;
+
+void reverse(int arr[50]){
+    // Base Case
+    if(i < 1)
+    {
+        cout<<sum;
+        return ;
+    }
+    // Recrance relation
+    
+}
+
+int main(){
+    int array[50];
+}
