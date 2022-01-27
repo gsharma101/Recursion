6ed2b6cabaddc54cb09016d131042f711e266980
gsharma101 <sharmag8121@gmail.com>
2022-01-27 23:48:40 +0530

    pallingdrome

---
+++ L3 Problems and Recursion/pallingdrome.cpp
@@ -0,0 +1,14 @@
+#include <bits\stdc++.h>
+// Program to check a string is a palindrome
+using namespace std;
+
+void palindrome(int i, int arr[], int n){
+    // Base Case
+    
+    // Recrance relation
+    palindrome(i+1, arr , n);  
+}
+
+int main(){
+   
+}
